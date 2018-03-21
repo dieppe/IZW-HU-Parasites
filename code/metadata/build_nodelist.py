1006cb1b7fafdf7e399cc6da0fdbf73cfc58aff0
Clément Vollet <clement@jnuine.com>
2018-03-21 17:14:26 +0100

    Refactor build nodelist
    Refactor the nodelist construction so it takes less time.
    Before this, for every node in the tree (2M items) we would:
    - search once in the parasite list (50K items)
    - search once in the freeliving list (40K items)
    - open the csv file, write one row, close the csv file.
    
    We now use two sets for constant time lookup of parasite/freeliving state of nodes. We write the complete nodelist at once at the end.
    
    We did not keep compatibility with previous version in that indexes are not kept between the tree and the csv file, but reading the csv file into a dictionnary should alleviate any lookup time problem anyway.
    
    Further work needs to be done though to actually implement this next part :)

--- code/metadata/build_nodelist.py
+++ code/metadata/build_nodelist.py
@@ -1,127 +1,152 @@
+import traceback
 import csv
 import datetime
 import sys
 from code.utilities.Helpers import print_time
-from code.utilities.nodelist_util import read_tags, tag_node
 from time import gmtime, strftime
 
 from Bio import Phylo
 from termcolor import colored
 
-# path_freelivings = "./data/interaction_data/reduced_freelivings.csv"
-# path_parasites = "./data/interaction_data/reduced_parasites.csv"
-path_freelivings = "./data/interaction_data/freelivings.csv"
-path_parasites = "./data/interaction_data/parasites.csv"
+freelivings_state_path = "./data/interaction_data/freelivings.csv"
+parasites_state_path = "./data/interaction_data/parasites.csv"
 
-# input arguments
-args = sys.argv
+def print_stats(stats):
+    print('Number of leaves:', stats['leaves'])
+    print('Number of parasites:', stats['tagged_as_P'])
+    print('Number of freelivings:', stats['tagged_as_FL'])
+    print('Number of double tags:', stats['doubly_tagged'])
+    print('Number of unknown:', stats['not_tagged'])
+    print('Percentage unkown:', stats['not_tagged'] / stats['leaves'])
 
-# values from  input:
-subtree_name = sys.argv[1]
+# Using set allow for very fast lookups
+def get_state_set(path):
+    state_set = set()
+    with open(path, 'r') as state_file:
+        state_file_reader = csv.reader(state_file, delimiter=',')
+        for row in state_file_reader:
+            if row != []: state_set.add("ott" + row[0])
+    print('number of tags', len(state_set))
+    return state_set
 
-# examples: 'Eukaryota'
-
-# global variables:
-START_TIME = datetime.datetime.now().replace(microsecond=0)
-CURRENT_TIME = datetime.datetime.now().replace(microsecond=0)
-freelivings = []
-parasites = []
-nr_leave_nodes = 0
-nr_used_freelivings = 0
-nr_used_parasites = 0
-unknown = 0
-doubleTagged = 0
-nodelist = []
+def load_known_states():
+    return {
+        'freelivings': get_state_set(freelivings_state_path),
+        'parasites': get_state_set(parasites_state_path),
+    }
 
 def main():
-    global START_TIME
-    global CURRENT_TIME
-    global freelivings
-    global parasites
-    global nr_leave_nodes
-    global nr_used_freelivings
-    global nr_used_parasites
-    global unknown
-    global nodelist
-    global doubleTagged
+    try:
+        _, subtree_name = sys.argv
+        stats = {
+            'leaves': 0,
+            'tagged_as_FL': 0,
+            'tagged_as_P': 0,
+            'not_tagged': 0,
+            'doubly_tagged': 0,
+        }
+        CURRENT_TIME = datetime.datetime.now().replace(microsecond=0)
 
-    print(colored("------------------------ build nodelists ------------------------", "green"))
-    print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-    CURRENT_TIME = print_time(START_TIME)
+        print(colored(strftime("[%Y-%m-%d %H:%M:%S] ", gmtime()) + "BUILDING NODELIST", "green"))
+        print(colored("- Loading freelivings and parasites states...", "green"))
+        known_states = load_known_states()
+        CURRENT_TIME = print_time(CURRENT_TIME)
 
-    print(colored("---------------- read parasites and freelivings ----------------", "green"))
-    print("Freelivings:")
-    freelivings = read_tags(path_freelivings)
-    print("Parasites:")
-    parasites = read_tags(path_parasites)
-    CURRENT_TIME = print_time(CURRENT_TIME)
-    print(colored("---------------- read tree ----------------", "green"))
-    subtree_path = './data/subtree/' + subtree_name + '.tre'
-    print("Build nodelist for:", subtree_name)
-    tree = Phylo.read(subtree_path, 'newick')
-    print(colored("---------------- tag tree ----------------", "green"))
-    fill_tree_with_tags(tree.clade, 0)
-    print(colored(nr_leave_nodes, 'blue'), "leave nodes are in the tree")
-    print(colored(nr_used_freelivings, 'blue'), "freeliving tags were used,", colored(nr_used_parasites, 'blue'), "parasite tags were used =>", colored(unknown, 'blue'), "unknown leave nodes")
-    print("Rootnode, Depth, Heigths: [Min, Max, Mean], Originaltag, Finaltag, Nr_children")
-    print(nodelist[0])
-    print(doubleTagged, "are tagged as P, but could also be FL!")
-    # ---- reset countings ----
-    nr_leave_nodes = 0
-    nr_used_freelivings = 0
-    nr_used_parasites = 0
-    unknown = 0
-    nodelist = []
-    CURRENT_TIME = print_time(CURRENT_TIME)
-    print(colored("--------------------------------", "green"))
+        print(colored('- Reading ' + subtree_name + ' subtree', "green"))
+        subtree_path = './data/subtree/' + subtree_name + '.tre'
+        tree = Phylo.read(subtree_path, 'newick')
+        CURRENT_TIME = print_time(CURRENT_TIME)
+
+        print(colored("- Tagging tree", "green"))
+        nodelist = []
+        build_nodelist_for_subtree_from_known_states(nodelist, tree.clade, known_states, 0, stats)
+        CURRENT_TIME = print_time(CURRENT_TIME)
+
+        print_stats(stats)
+        print("Rootnode, Depth, Heigths: [Min, Max, Mean], Originaltag, Finaltag, Nr_children")
+        print(nodelist[0])
+
+        csv_path = './data/nodelist/' + subtree_name + '.csv'
+        print(colored("Writing csv file to " + csv_path, "green"))
+        with open(csv_path, 'a') as nodelist_file:
+            writer = csv.writer(nodelist_file)
+            writer.writerows(nodelist)
+        CURRENT_TIME = print_time(CURRENT_TIME)
+    except:
+        print(colored("Unexpected error: ", "red"))
+        traceback.print_exc()
     return
 
-def fill_tree_with_tags(subtree, depth):
-    global nr_leave_nodes
-    global nr_used_freelivings
-    global nr_used_parasites
-    global unknown
-    global nodelist
-    global doubleTagged
-    
-    ott = subtree.name.split("$")[0] # remove index
-    heights = [1, 1, 1]
-    
+def build_nodelist_for_subtree_from_known_states(nodelist, subtree, known_states, depth, stats):
+    ott = subtree.name
     #                   0    1              2       3       4           5
     # nodelist      - [id, originaltag, finaltag, depth, heights, nr_children]
-    nodelist.append([ott, "", "", depth, heights, len(subtree.clades)])
+    nodelist.append([ott, "", "", depth, None, len(subtree.clades)])
     current_list_index = len(nodelist) - 1
 
     if subtree.is_terminal():
-        stats = [nr_leave_nodes, nr_used_parasites, nr_used_freelivings, unknown, doubleTagged]
-        stats = tag_node(nodelist, current_list_index, ott, [freelivings, parasites], stats)
-        nr_leave_nodes = stats[0]
-        nr_used_parasites = stats[1]
-        nr_used_freelivings = stats[2]
-        unknown = stats[3]
-        doubleTagged = stats[4]
+        tag_leaf(nodelist, current_list_index, ott, known_states, stats)
+        # min height, max height, mean height at leaf level
+        heights = [1, 1, 1]
     else:
-        min_heigth = float('inf')
-        max_heigth = 0
-        mean_heigth = 0
-        child_heigth = 0
+        min_height, max_height, mean_height, child_height = [float('inf'), 0, 0, 0]
         for clade in subtree.clades:
-            heights = fill_tree_with_tags(clade, depth + 1)
-            if heights[0] < min_heigth:
-                min_heigth = heights[0]
-            if heights[1] > max_heigth:
-                max_heigth = heights[1]
-            child_heigth = child_heigth + heights[2]
-        mean_heigth = child_heigth/len(subtree.clades) + 1
-        heights = [min_heigth + 1, max_heigth + 1, mean_heigth]
-        nodelist[current_list_index][4] = heights
-    # -------------------------------------------------
-    csv_title = './data/nodelist/' + subtree_name + '.csv' 
-    nodelist_file = open(csv_title, 'a')
-    writer = csv.writer(nodelist_file)
-    writer.writerow((nodelist[current_list_index])) 
-    nodelist_file.close()
+            # Build nodelist for children first
+            heights = build_nodelist_for_subtree_from_known_states(
+                nodelist, clade, known_states, depth + 1, stats
+            )
+            if heights[0] < min_height:
+                # Min height of the child is lower than current min height
+                min_height = heights[0]
+            if heights[1] > max_height:
+                # Max height of the child is greater than current max height
+                max_height = heights[1]
+            # Add the child mean height to the current sum of all children mean height
+            child_height = child_height + heights[2]
+        # Divide the sum of children mean heights by the number of children
+        mean_height = child_height/len(subtree.clades)
+        heights = [min_height + 1, max_height + 1, mean_height + 1]
+    nodelist[current_list_index][4] = heights
     # -------------------------------------------------
     return heights
+
+def tag_leaf(nodelist, current_list_index, ott, states, stats):
+    #                   0    1              2       3       4           5
+    # nodelist      - [id, originaltag, finaltag, depth, heights, nr_children]
+    # current_list_index - index of current node
+    # ott
+    # species_lists - [freelivings, parasites]
+    # stats         - [number_of_leaves, number_of_tagged_P, number_of_tagged_FL, number_of_unknown, number_of_double_tags]
+    stats['leaves'] += 1
+
+    flag = "NA"
+    is_parasite = is_in_state_set(ott, states['parasites'])
+    is_freeliving = is_in_state_set(ott, states['freelivings'])
+
+    if is_parasite:
+        print(ott, 'is parasite')
+        flag = "2"
+        stats['tagged_as_P'] += 1
+    elif is_freeliving:
+        print(ott, 'is freeliving')
+        flag = "1"
+        stats['tagged_as_FL'] += 1
+    else:
+        # print(ott, 'is unknown')
+        stats['not_tagged'] += 1
+
+    if is_parasite and is_freeliving:
+        print(ott, 'is actually both')
+        stats['tagged_as_P'] -= 1
+        stats['doubly_tagged'] += 1
+
+    nodelist[current_list_index][1] = flag
+    return
+
+def is_in_state_set(name, states):
+    if name in states:
+        states.remove(name)
+        return True
+    return False
 
 main()
