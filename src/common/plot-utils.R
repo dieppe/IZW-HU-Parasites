b96e12cf0ca297258adabc3946c13402838bf8ae
Clément Vollet <cvollet@gmail.com>
2018-08-29 12:49:16 +0200

    Fix plot-utils to work with new data structures

--- src/common/plot-utils.R
+++ src/common/plot-utils.R
@@ -3,87 +3,113 @@ library('modules')
 CONFIG <- import('./config')
 utils <- import('./utils')
 
-utils$install_packages(c('ggplot2', 'reshape2'))
+utils$install_packages('ggplot2')
 ggplot <- import_package('ggplot2')
-reshape <- import_package('reshape2')
 
 plot_results <- Vectorize(
-  function (clade_name, evaluation_results_for_clade, stat_results_for_clade, drops) {
-    melted_results <- reshape$melt(evaluation_results_for_clade)
-    melted_stats <- reshape$melt(stat_results_for_clade)
-  
+  function (
+    clade_name, 
+    evaluation_results_for_clade, 
+    stat_results_for_clade, 
+    drops
+  ) {
+    # One-liner from https://stackoverflow.com/a/12639740
+    melted_results <- data.frame(
+      X = rep(
+        as.numeric(names(evaluation_results_for_clade)),
+        sapply(evaluation_results_for_clade, length)
+      ), 
+      Y = unlist(evaluation_results_for_clade)
+    )
+    
     plot <- ggplot$ggplot(
         melted_results,
         ggplot$aes(
-          x = Var2,
-          y = value
+          x = X,
+          y = Y
         )
       ) +
       ggplot$geom_point(size = 0.1, color = "#ff967f") +
       ggplot$theme_bw() +
       ggplot$theme(
         text = ggplot$element_text(
-          size=20, 
-          family = "Phosphate Inline"
+          size=20,
+          # family = "Phosphate Inline"
+          family = "Optima"
         ),
-        title = ggplot$element_text(
-          colour = ggplot$alpha(colour = "#ff967f", 0.8)
-        ),
+        # title = ggplot$element_text(
+        #   colour = ggplot$alpha(colour = "#ff967f", 0.8)
+        # ),
         axis.text = ggplot$element_text(
-          size=10, 
-          family = "Optima", 
-          colour = ggplot$alpha(colour = "#000000", 0.6)
+          size=12,
+          family = "Optima"
+          # colour = ggplot$alpha(colour = "#000000", 0.6)
         ),
         axis.title = ggplot$element_text(
-          size=14, 
-          family = "Optima", 
-          colour = ggplot$alpha(colour = "#000000", 0.3)
-        ),
-        plot.background = ggplot$element_rect(
-          fill = ggplot$alpha(colour = "#7fffd9", 0.3)
-        ),
-        panel.border = ggplot$element_rect(
-          color = ggplot$alpha("#7fffd9", 0.3), 
-          size = 0.2
-        ),
-        panel.background = ggplot$element_rect(
-          fill = ggplot$alpha(colour = "#ffffff", 0.8), 
-          color = "white"
-        ),
-        panel.grid.major = ggplot$element_line(
-          color = ggplot$alpha("#7fffd9", 0.4),
-          size = 0.2
-        ),
-        panel.grid.minor = ggplot$element_line(
-          color = ggplot$alpha("#7fffd9", 0.4), 
-          size = 0.2, 
-          linetype = "dashed"
+          size=16,
+          family = "Optima"
+          # colour = ggplot$alpha(colour = "#000000", 0.3)
         )
+        # plot.background = ggplot$element_rect(
+        #   fill = ggplot$alpha(colour = "#7fffd9", 0.3)
+        # ),
+        # panel.border = ggplot$element_rect(
+        #   color = ggplot$alpha("#7fffd9", 0.3),
+        #   size = 0.2
+        # ),
+        # panel.background = ggplot$element_rect(
+        #   fill = ggplot$alpha(colour = "#ffffff", 0.8),
+        #   color = "white"
+        # ),
+        # panel.grid.major = ggplot$element_line(
+        #   color = ggplot$alpha("#7fffd9", 0.4),
+        #   size = 0.2
+        # ),
+        # panel.grid.minor = ggplot$element_line(
+        #   color = ggplot$alpha("#7fffd9", 0.4),
+        #   size = 0.2,
+        #   linetype = "dashed"
+        # )
       ) +
       ggplot$scale_x_continuous(
-        name = "dropped", 
-        breaks = round(seq(drops[1], drops[length(drops)], length.out = 10), 1)
+        name = "% known states dropped",
+        breaks = seq(0, 100, length.out = 11),
+        labels = seq(0, 100, length.out = 11)
       ) +
       ggplot$scale_y_continuous(
-        name = "recall", 
-        breaks = 0:10 / 10, 
-        labels = 0:10 * 10
+        name = "Accuracy"
+        # breaks = 0:10 / 10
+        # labels = 0:10 * 10
       ) +
       ggplot$geom_line(
-        data = melted_stats[melted_stats$Var2 == "min",],
+        data = stat_results_for_clade,
         ggplot$aes(
-          x = Var1,
-          y = value
+          x = drop,
+          y = min,
+          group = 1
         ),
-        col = "#ffd67f"
+        col = "#ffd67f",
+        size = 1
       ) +
       ggplot$geom_line(
-        data = melted_stats[melted_stats$Var2 == "max",],
+        data = stat_results_for_clade,
         ggplot$aes(
-          x = Var1,
-          y = value
+          x = drop,
+          y = max,
+          group = 1
         ),
-        col = "#ac7fff"
+        col = "#ac7fff",
+        size = 1
+      ) +
+      ggplot$geom_line(
+        data = stat_results_for_clade,
+        ggplot$aes(
+          x = drop,
+          y = mean,
+          group = 1
+        ),
+        col = "#7FE7FF",
+        size = 1
       ) +
       ggplot$ggtitle(toupper(clade_name))
     return(plot)
